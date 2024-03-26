0769e7f675d86230d6159f3819ce8e8f7357ebd2
Prathmesh Gawade <gawadepr101@gmail.com>
2024-03-26 12:42:26 +0530

    add

--- src/index.js
+++ src/index.js
@@ -27,6 +27,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// t log results (for example: reportWebVitals(console.log))
+// /int. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
